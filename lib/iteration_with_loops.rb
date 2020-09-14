7b262e4c59899d05d53d473af438718da8ae49ed
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-14 18:46:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_even_values(src)
       if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
-      element_index =+ 1
+      element_index += 1
     end
     row_index += 1
   end
